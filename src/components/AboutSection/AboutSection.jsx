1e85df2c8120b6634e518c170d61497fad970ca9
Shiva-yj <shivaneshanyj2008@gmail.com>
2026-08-20 19:16:36 +0530

    new

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='/Photo.jpeg' alt='Rakesh' />
+          <img src='/myimg.png' alt='Shivaneshan Y J' />
         </div>
         <div className='about-copy'>
           <h1>About
